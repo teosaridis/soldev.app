1108b00c3b115288e8df9c5b96b6b3390355a847
Theo <tsaridakis@gmail.com>
2024-07-14 16:52:21 +0300

    Fixing structure

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,21 @@
+// import { Keypair } from "@solana/web3.js";
+
+// const keypair = Keypair.generate();
+
+// console.log(`The public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
+// console.log(`✅ Finished!`);
+
+require('dotenv').config()
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
+
+console.log(
+    keypair
+  );
